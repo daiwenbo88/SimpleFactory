bb9218ca6dce39f8d925441c05a987592990405d
代文波 <daiwwenb@163.com>
2017-02-14 23:31:44 +0800

    添加观察者模式

--- DesignMode/app/src/main/java/com/example/daiwenbo/designmode/MainActivity.java
+++ DesignMode/app/src/main/java/com/example/daiwenbo/designmode/MainActivity.java
@@ -10,10 +10,14 @@ import com.example.daiwenbo.designmode.factory.bean.Role;
 import com.example.daiwenbo.designmode.factory.bean.User;
 import com.example.daiwenbo.designmode.factory.dao.role.IRoleDao;
 import com.example.daiwenbo.designmode.factory.dao.user.IUserDao;
+import com.example.daiwenbo.designmode.observer.Architect;
+import com.example.daiwenbo.designmode.observer.ITalent;
+import com.example.daiwenbo.designmode.observer.JuniorEngineer;
 import com.example.daiwenbo.designmode.simpefactory.BuleCar;
 import com.example.daiwenbo.designmode.simpefactory.Car;
 import com.example.daiwenbo.designmode.simpefactory.CarFactory;
 import com.example.daiwenbo.designmode.simpefactory.CarFactory2;
+import com.example.daiwenbo.designmode.subject.HeadHunter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected final static String RED="红色";
     protected final static String BACK="黑色";
     protected final static String BULE="蓝色";
+    private TextView tv_observice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         Car car = CarFactory.newCar(RED);
         tv_text =(TextView)findViewById(R.id.tv_text);
+        tv_observice =(TextView)findViewById(R.id.tv_observice);
         tv_text.setText(car.drive());
 
         car= CarFactory2.newCar(BuleCar.class);
@@ -41,5 +47,15 @@ public class MainActivity extends AppCompatActivity {
         IRoleDao roleDao = new SqlServiceDaoFactory().createRoleDao();
         Role role = roleDao.getUser("抽象工厂方法");
         tv_text.setText(role.getName());
+
+        //观察者模式
+        final ITalent iTalent=new Architect();
+        ITalent iTalent1=new JuniorEngineer();
+        final HeadHunter headHunter=new HeadHunter();
+
+        headHunter.addTalent(iTalent);
+        headHunter.addTalent(iTalent1);
+
+        headHunter.publishJob("洗碗");
     }
 }
